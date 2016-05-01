c4b775f441eaeaec6049028bf2ff8cc451904301
ANRU-Corp <antoni@rucinski.pol.pl>
2016-05-01 16:37:02 +0200

    with translation

--- BombaAtomowa/src/pl/pw/edu/fizyka/pojawa/TMKJKS/MenuPanel.java
+++ BombaAtomowa/src/pl/pw/edu/fizyka/pojawa/TMKJKS/MenuPanel.java
@@ -1,87 +1,88 @@
+
 package pl.pw.edu.fizyka.pojawa.TMKJKS;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
+
+
 public class MenuPanel extends JFrame { //by Antoni Rucinski
 
+	
 	private static final long serialVersionUID = 1L;
 	public Options options = new Options();
 	public boolean boolSimulate=false;
 	boolean yesNoSimulation=true;
 	JMenuItem startStopMenuItem = new JMenuItem("Start / Stop");
 
+	
+	private ResourceBundle resourceBundle = ResourceBundle.getBundle(
+			"pl/pw/edu/fizyka/pojawa/TMKJKS/labels",new Locale(ChooseLanguage.getLocal()));
+
 	public JMenuBar createMenu(){
-		
-		
+			
 		JMenuBar menuBar = new JMenuBar();	//building main menu
-		JMenu menu = new JMenu("Plik");
+		JMenu menu = new JMenu(resourceBundle.getString("menu.mainMenu"));
 		menuBar.add(menu);
 
-		final JMenuItem saveMenuItem = new JMenuItem("Zapisz");
+		//save menu Item
+		final JMenuItem saveMenuItem = new JMenuItem(resourceBundle.getString("menu.saveMenuItem"));
 		menu.add(saveMenuItem);
 		menu.addSeparator();
 		
-		JMenu submenu = new JMenu("Język");
-		
-		final JMenuItem polishLanguageMenuItem = new JMenuItem("polski");
-		submenu.add(polishLanguageMenuItem);
-		submenu.addSeparator();	
-		
-		final JMenuItem englishLanguageMenuItem= new JMenuItem("angielski");
-		submenu.add(englishLanguageMenuItem);
-		menu.add(submenu);
-		
-		menu.addSeparator();
-		final JMenuItem closeMenuItem = new JMenuItem("Zamknij");
+		//close menu item
+		final JMenuItem closeMenuItem = new JMenuItem(resourceBundle.getString("menu.closeMenuItem"));
 		menu.add(closeMenuItem);
 		
-		menu = new JMenu("Symulacja");
+		//simulation menu
+		menu = new JMenu(resourceBundle.getString("menu.simulationMenu"));
 		
-		//final JMenuItem startStopMenuItem = new JMenuItem("Start / Stop");
+		//start stop menu item
+		final JMenuItem startStopMenuItem = new JMenuItem(resourceBundle.getString("menu.startStopMenuItem"));
 		menu.add(startStopMenuItem);
-		submenu.addSeparator();
-		
-		final JMenuItem symulationOptionMenuItem = new JMenuItem("Opcje Symulacji");
+		menu.addSeparator();
+	
+		//simulation options menu item
+		final JMenuItem symulationOptionMenuItem = new JMenuItem(resourceBundle.getString("menu.symulationOptionsMenuItem"));
 		menu.add(symulationOptionMenuItem);
 
 		menuBar.add(menu);
-		
+
+//Listeners
 		ActionListener menuListener=new ActionListener(){
+			
 			public void actionPerformed(ActionEvent e){
 				if(e.getSource()==closeMenuItem){
-					System.out.println("Zamkni�to program");
+					System.out.println("Zamknięto program");
 		            System.exit(1);
 				}
 				else if(e.getSource()==saveMenuItem){
 					System.out.println("Plik zapisano");
 					//TODO Make a saving instruction
 				}
-				else if(e.getSource()==polishLanguageMenuItem){
-					System.out.println("Zmiana języka na polski");  
-					//TODO Make a changing language instruction
-				}
-				else if(e.getSource()==englishLanguageMenuItem){
-					System.out.println("Zmiana języka na polski");  
-					//TODO Make a changing language instruction
-				}
+
 				else if(e.getSource()==symulationOptionMenuItem){
 					System.out.println("Uruchomiono okienko opcji symulacji"); 
-					//Options opcje = new Options();
-					options.setVisible(true);
+					Options opcje = new Options();
+					opcje.setVisible(true);
+				}
+				else if(e.getSource()==startStopMenuItem){
+					System.out.println("uruchomiono okienko opcji symulacji");
+					//TODO Make an option window instruction
 				}
 			}
 		};
 		
 		closeMenuItem.addActionListener(menuListener);
 		saveMenuItem.addActionListener(menuListener);
-		polishLanguageMenuItem.addActionListener(menuListener);
-		englishLanguageMenuItem.addActionListener(menuListener);
+	
 		symulationOptionMenuItem.addActionListener(menuListener);
 		startStopMenuItem.addActionListener(menuListener);
 
@@ -89,3 +90,4 @@ public class MenuPanel extends JFrame { //by Antoni Rucinski
 	}
 	
 }
+
