6dab99c1e11b342ce2357dc9fb65c4b2b50c893a
ANRU-Corp <antoni@rucinski.pol.pl>
2016-05-01 16:36:00 +0200

    with translation

--- BombaAtomowa/src/pl/pw/edu/fizyka/pojawa/TMKJKS/Interface.java
+++ BombaAtomowa/src/pl/pw/edu/fizyka/pojawa/TMKJKS/Interface.java
@@ -10,18 +10,22 @@ import javax.swing.JLabel;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
+import java.util.Locale;
 import java.util.Random;
+import java.util.ResourceBundle;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Interface extends JFrame { //by Antoni Ruciñski & Jacek Pi³ka
+public class Interface extends JFrame { //by Antoni Rucinski & Jacek Pilka
 	
 	private static final long serialVersionUID = 185723979423401295L;
 	Random rand = new Random();
 	static JFrame window = new Interface();
+	private ResourceBundle resourceBundle = ResourceBundle.getBundle(
+			"pl/pw/edu/fizyka/pojawa/TMKJKS/labels",new Locale(ChooseLanguage.getLocal()));
 
 //interface constructor: creating main frame with a menu
 	public Interface() throws HeadlessException {
@@ -29,7 +33,7 @@ public class Interface extends JFrame { //by Antoni Ruciñski & Jacek Pi³ka
 		setSize(800,600);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLayout(new BorderLayout());
-		setTitle("Symulator Bomby Atomowej");
+		setTitle(resourceBundle.getString("interface.title"));
 		
 		
 		JPanel manePanel = new JPanel();
@@ -41,20 +45,20 @@ public class Interface extends JFrame { //by Antoni Ruciñski & Jacek Pi³ka
 		manePanel.setLayout(new GridLayout(1,2));
 		bottomPanel.setLayout(new GridLayout(1, 5));
 		
-		//podzia³ na kolumny
+		//podzial na kolumny
 		manePanel.add(new Mock("Wykres zmian energii od czasu"));
 		manePanel.add(new Mock("Wykres zmian energii od czasu"));
 		
-		JTextField maxEnergy = new JTextField("");
-		JTextField Energy = new JTextField("");
+		final JTextField maxEnergy = new JTextField("");
+		final JTextField Energy = new JTextField("");
 		
-		bottomPanel.add(new JLabel("Energia maksymalna[J]:"));
+		bottomPanel.add(new JLabel(resourceBundle.getString("interface.maxEnergy")));
 		bottomPanel.add(maxEnergy);
 		bottomPanel.add(new JLabel());
-		bottomPanel.add(new JLabel("Energia obecnie[J]:"));
+		bottomPanel.add(new JLabel(resourceBundle.getString("interface.currentEnergy")));
 		bottomPanel.add(Energy);
 		
-		MenuPanel menuPanel = new MenuPanel();
+		final MenuPanel menuPanel = new MenuPanel();
 		setJMenuBar(menuPanel.createMenu());
 		
 		menuPanel.startStopMenuItem.addActionListener(new ActionListener(){//Start simulation
@@ -65,7 +69,7 @@ public class Interface extends JFrame { //by Antoni Ruciñski & Jacek Pi³ka
 					//exec.execute(simulation);
 					simulation.run();
 					if(simulation.neutrons.size()<1){
-						System.out.println("Brak neutronów!");
+						System.out.println("Brak neutronÃ³w!");
 						break;
 					}
 					Energy.setText(Float.toString(simulation.energy));
@@ -78,9 +82,14 @@ public class Interface extends JFrame { //by Antoni Ruciñski & Jacek Pi³ka
 			}
 		});
 	}
+
 	
 	public static void main(String[] args) {
+		ChooseLanguage.ChooseLanguage();
 		JFrame f = new Interface();
 		f.setVisible(true);
 	}
 }
+
+
+
