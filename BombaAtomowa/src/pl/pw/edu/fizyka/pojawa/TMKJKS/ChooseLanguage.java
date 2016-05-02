a3af692b42c0cee3e9d12077d8a29bed8c1eff3b
ANRU-Corp <antoni@rucinski.pol.pl>
2016-05-02 09:40:52 +0200

    new class: choose language

---
+++ BombaAtomowa/src/pl/pw/edu/fizyka/pojawa/TMKJKS/ChooseLanguage.java
@@ -0,0 +1,38 @@
+package pl.pw.edu.fizyka.pojawa.TMKJKS;
+
+
+
+
+
+import java.awt.HeadlessException;
+
+
+import javax.swing.JOptionPane;
+
+public class ChooseLanguage { //by Antoni Rucinski
+	
+	static int l;
+	public static String local="pl";
+	
+	public static void ChooseLanguage() throws HeadlessException {
+	
+		  Object[] options = { "Polski", "English" };
+		  
+		  l = JOptionPane.showOptionDialog(null, "Wybierz Język / Choose your language", "Język",
+		  JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE,
+		  null, options, options[0]);
+		  
+		  System.out.println(l);
+		  if(l==0){
+			  local="pl";
+		  }
+		  else if(l==1){
+			  local="en";
+		  }
+	}
+	
+	static String getLocal(){
+		return local;
+	}
+	
+}
