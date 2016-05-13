ccda86c9af18e7a7cc4c5849334ed883efd192c1
Antoni Rucinski <antoni@rucinski.pol.pl>
2016-05-13 16:41:21 +0200

    new class: warning dialog box

---
+++ BombaAtomowa/src/pl/pw/edu/fizyka/pojawa/TMKJKS/ClosingWarning.java
@@ -0,0 +1,48 @@
+package pl.pw.edu.fizyka.pojawa.TMKJKS;
+import java.awt.HeadlessException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.swing.JOptionPane;
+
+public class ClosingWarning { //by Antoni Rucinski
+	
+	static int l;
+	public static int local;
+	
+	private static ResourceBundle resourceBundle = ResourceBundle.getBundle(
+			"pl/pw/edu/fizyka/pojawa/TMKJKS/labels",new Locale(ChooseLanguage.getLocal()));
+	
+	
+	
+	public static void ClosingWarning() throws HeadlessException {
+	
+		  Object[] options = { "OK", resourceBundle.getString("cWarning.Correct")};
+		  
+		  l = JOptionPane.showOptionDialog(null, resourceBundle.getString("cWarning.message"), 
+				  resourceBundle.getString("cWarning.title"),
+		  JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE,
+		  null, options, options[0]);
+		  
+		  System.out.println("Źle wypełnione okienko opcji: "+l);
+		  //OK
+		  if(l==0){
+			  local=0;
+			  System.out.println("Closing Warning: Akceptacja źle uzupełnionych opcji.");
+		  }
+		  //Popraw dane
+		  else if(l==1){
+			  System.out.println("ClosingWarning: Ponowne uzupełnienie opcji");
+				System.out.println("Uruchomiono okienko opcji symulacji"); 
+				MenuPanel.options.setVisible(true);
+				
+				
+			
+		  }
+	}
+	
+	static int getLocal(){
+		return local;
+	}
+	
+}
